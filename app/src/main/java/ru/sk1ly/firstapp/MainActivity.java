8558d14ce2cd09b032b5fe099fca746000ef25f5
sk1ly <io.skvortsov@mail.ru>
2019-11-25 23:38:46 +0300

    FIRSTAPP-003 Добавил альбомную ориентацию и несколько кнопок + поправил макеты

--- app/src/main/java/ru/sk1ly/firstapp/MainActivity.java
+++ app/src/main/java/ru/sk1ly/firstapp/MainActivity.java
@@ -1,9 +1,11 @@
 package ru.sk1ly.firstapp;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +16,10 @@ import static ru.sk1ly.firstapp.AuthorizeActivity.USER_SECOND_NAME_KEY;
 public class MainActivity extends AppCompatActivity {
 
     private static final int AUTHORIZE_USER = 0;
+    private static final String KEK_WORD = "Kek";
+    private static final String PORTRAIT_ORIENTATION_WORD_RUS = "портретная";
+    private static final String LANDSCAPE_ORIENTATION_WORD_RUS = "альбомная";
+    private static final String UNKNOWN_ORIENTATION_WORD_RUS = "неизвестная";
 
     private TextView mUserFirstName;
     private TextView mUserSecondName;
@@ -40,5 +46,29 @@ public class MainActivity extends AppCompatActivity {
     public void onClickAuthorizeButton(View view) {
         Intent authorizeIntent = new Intent(MainActivity.this, AuthorizeActivity.class);
         startActivityForResult(authorizeIntent, AUTHORIZE_USER);
+    }
+
+    public void onClickSayKekButton(View view) {
+        Toast.makeText(this, KEK_WORD, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickGetOrientationButton(View view) {
+        int currentOrientation = getResources().getConfiguration().orientation;
+        if (currentOrientation == Configuration.ORIENTATION_PORTRAIT) {
+            makeOrientationToastText(PORTRAIT_ORIENTATION_WORD_RUS);
+        } else if (currentOrientation == Configuration.ORIENTATION_LANDSCAPE) {
+            makeOrientationToastText(LANDSCAPE_ORIENTATION_WORD_RUS);
+        } else {
+            makeOrientationToastText(UNKNOWN_ORIENTATION_WORD_RUS);
+        }
+    }
+
+    public void onClickAboutAppButton(View view) {
+        //        TODO
+        Toast.makeText(this, "TODO", Toast.LENGTH_SHORT).show();
+    }
+
+    private void makeOrientationToastText(String orientationType) {
+        Toast.makeText(this, "В данный момент " + orientationType + " ориентация", Toast.LENGTH_LONG).show();
     }
 }
