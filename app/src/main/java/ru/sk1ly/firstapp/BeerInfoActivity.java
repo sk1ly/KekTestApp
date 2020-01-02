ca66e9078a67ca2c58e04cb4b9ba200b7a119d78
sk1ly <io.skvortsov@mail.ru>
2020-01-03 00:38:33 +0300

    FIRSTAPP-006 Добавил активити BeerInfoActivity и ее макет с некоторыми наработками

---
+++ app/src/main/java/ru/sk1ly/firstapp/BeerInfoActivity.java
@@ -0,0 +1,14 @@
+package ru.sk1ly.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BeerInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beer_info);
+    }
+}
